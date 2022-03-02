82b7fd376e48ee5bbf0d4aaf8bbc9343f746b8a5
plymatea <andy.plymate@gmail.com>
2022-03-02 09:51:53 -0800

    add initial environment
    co-authored-by: Matt Eilar <meilar@gmail.com>

---
+++ Places/Controllers/PlacesController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Places.Models;
+using System.Collections.Generic;
+
+namespace Places.Controllers
+{
+  public class PlacesController : Controller
+  {
+
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
+
+    // [HttpGet("/items/new")]
+    // public ActionResult CreateForm()
+    // {
+    //   return View();
+    // }
+
+    // [HttpPost("/items")]
+    // public ActionResult Create(string description)
+    // {
+    //   Item myItem = new Item(description);
+    //   return RedirectToAction("Index");
+    // }
+  }
+}
---
+++ Places/Models/Places.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using System;
+
+namespace Places.Models
+{
+  public class Place
+  {
+    
+  }
+}
